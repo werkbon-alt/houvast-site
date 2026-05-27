785ee2ac65b322a58299f6cde409d1770038f455
werkbon-alt <werkbon@houvast-ontzorgen.net>
2026-05-27 23:49:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
 export default function App() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Houvast website werkt</h1>
-      <p>Als je dit ziet, draait de website goed.</p>
-    </main>
-  );
+  return <h1>TEST HOUVAST WEBSITE</h1>;
 }
